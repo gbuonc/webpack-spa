e80a22c0f852bc1f3d8fddf141a7010d55bac109
Gianluca Buoncompagni <gbuonc@gmail.com>
2016-07-21 18:19:41 +0200

    add new https json source

--- dev/config.js
+++ dev/config.js
@@ -1,6 +1,6 @@
 var app = {
     title : 'Il Fatto Quotidiano - Web App',
-    contentEndPoint : 'https://wp40.ilfattoquotidiano.it/premium/edizione/mercoledi-22-giugno-2016/?json=1',
+    contentEndPoint : 'https://wp40.ilfattoquotidiano.it/premium/edizione/giovedi-21-luglio-2016/?json=1',
     offline: {
         serviceWorker : true,
         appCache: true
